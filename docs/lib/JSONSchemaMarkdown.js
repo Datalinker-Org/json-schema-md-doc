81445899c4e2bce149e7488bc9a0fd965b322753
phillipsp73 <54051387+phillipsp73@users.noreply.github.com>
2020-03-24 13:51:56 +1300

    Update JSONSchemaMarkdown.js

--- docs/lib/JSONSchemaMarkdown.js
+++ docs/lib/JSONSchemaMarkdown.js
@@ -154,7 +154,7 @@ class JSONSchemaMarkdown {
         this.writeExamples(data.examples, level, path);
         this.writeEnum(data.enum, level);
         this.writeDefault(data.default, level, path);
-        this.writeId(data["@id"], level, path);
+        /*this.writeId(data["@id"], level, path);*/
         this.writeId(data.dltype, level, path);
        }
 
