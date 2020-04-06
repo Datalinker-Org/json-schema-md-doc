1df3abce5ef7980f008629d096ebd8fe798a2ee8
AndyDe-Aldent <andrew.de.lange@rezare.com>
2020-04-07 09:28:47 +1200

    Trying to get the async functions to work

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const github = require('@actions/github');
 const glob = require('@actions/glob');
 
 try {
+  runMDGeneration();
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+async function runMDGeneration(){
   // Creates to globber to search through the repository for the JSON files
-  const patterns = ['**/json']
-  const globber = await glob.create(patterns.join('\n'))
+  const patterns = ['**/json'];
+  const globber = glob.create(patterns.join('\n'));
 
   // hopefully if this works right should iterate of the list of all the JSON files in the repository
-  for await (const file of globber.globGenerator()){
-    console.log(file)
+  for (const file of globber.globGenerator()){
+    console.log(file);
   }
 
   // TODO: Save the MD files from the generator
-} catch (error) {
-  core.setFailed(error.message);
 }
