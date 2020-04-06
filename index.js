3f06f440afb375570274a677b9e4f6f17fdba569
AndyDe-Aldent <andrew.de.lange@rezare.com>
2020-04-07 09:30:11 +1200

    forgot the awaits

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ try {
 async function runMDGeneration(){
   // Creates to globber to search through the repository for the JSON files
   const patterns = ['**/json'];
-  const globber = glob.create(patterns.join('\n'));
+  const globber = await glob.create(patterns.join('\n'));
 
   // hopefully if this works right should iterate of the list of all the JSON files in the repository
-  for (const file of globber.globGenerator()){
+  for await (const file of globber.globGenerator()){
     console.log(file);
   }
 
